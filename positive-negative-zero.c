f5825e3337511035155e64e02d2acce85b75b0f7
Jayakumar-K <jayakumar9595@gmail.com>
2016-06-06 22:51:41 +0530

    Update positive-negative-zero.c

--- positive-negative-zero.c
+++ positive-negative-zero.c
@@ -1,1 +1,18 @@
-
+#include<stdio.h>
+main()
+{
+int n;
+scanf("%d",&n);
+if(n>0)
+ {
+ printf("positive");
+}
+  else if(n<0)
+   {
+  printf("negative");
+}
+    else
+    {
+  printf("zero");
+}
+}
